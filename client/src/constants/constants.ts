87e9342c8de09ab8830d03dee0a8bf786f1bb6a1
Pranjal Gain <pranjalgain052@gmail.com>
2025-06-05 17:36:37 +0530

    fix: Revert base URL to local development environment

--- client/src/constants/constants.ts
+++ client/src/constants/constants.ts
@@ -1,5 +1,5 @@
-// export const baseUrl: string = "http://localhost:3000/api/v1";
-export const baseUrl: string = "https://resource-manager-beta.vercel.app/api/v1";
+export const baseUrl: string = "http://localhost:3000/api/v1";
+// export const baseUrl: string = "https://resource-manager-beta.vercel.app/api/v1";
 export const calculateCapacity = (
     allEngineers: any[],
     engineer: any,
